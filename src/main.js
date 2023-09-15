5c0aab3b4153d5a0e6c66e75793e25e58caed77c
tomas gramantas <tgramantas@gmail.com>
2023-09-15 16:15:04 +0100

    first commit

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import '../styles/style.css';
+import '../styles/components/header.css'
+import '../styles/components/hero.css'
+import '../styles/components/about.css'
+import '../styles/components/projects.css'
+import '../styles/components/contact.css'
+import '../styles/components/mobile-nav.css'
+import '../styles/utils.css';
+
+import mobileNav from './utils/mobile-nav';
+
+mobileNav();
